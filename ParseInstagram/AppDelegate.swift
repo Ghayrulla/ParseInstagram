cd004acaf5d63d42282e668376d452faca665fc9
ghayrulla <ghayrullaghayrat@hotmail.com>
2016-03-13 23:41:29 -0400

    app updated

--- ParseInstagram/AppDelegate.swift
+++ ParseInstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstagramParse"
+                configuration.clientKey = "thisisasecretmasterkey"
+                configuration.server = "https://frozen-spire-54912.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
---
+++ ParseInstagram/LoginViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LoginViewController.swift
+//  ParseInstagram
+//
+//  Created by Gary Ghayrat on 3/13/16.
+//  Copyright © 2016 Gary Ghayrat. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onSignIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                print("you're logged in")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(sender: AnyObject) {
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)  
+                if error?.code == 202 {
+                    print("user name is taken")
+                }
+                // manually segue to logged in view
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
